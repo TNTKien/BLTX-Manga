e41f432f812c8d74016c81375fdde41b011d469e
iamneyk <iamneyk@gmail.com>
2024-04-18 00:41:19 +0700

    fix random manga

--- src/components/Manga/RandomManga.tsx
+++ src/components/Manga/RandomManga.tsx
@@ -1,19 +1,13 @@
 import CarouselManga from "./components/CarouselManga";
 import { db } from "@/lib/db";
+import prismaRandom from 'prisma-extension-random';
+import { Manga } from "@prisma/client";
+
+
 
 const RandomManga = async () => {
-    const mangas = await db.manga.findMany({
-        take: 10,
-        orderBy: {
-            title: 'asc',
-        },
-        select: {
-            id: true,
-            title: true,
-            description: true,
-            cover: true,
-        },
-    });
+    const mangas = shuffleMangas(await pickRandomMangas(10));
+
     const jsonLd = {
         '@context': 'https://schema.org',
         '@type': 'ItemList',
@@ -35,5 +29,30 @@ const RandomManga = async () => {
         </>
     )
 };
+
+async function pickRandomMangas(count: number): Promise<Manga[]> {
+    const mCount = await db.manga.count();
+    const skip = Math.max(0, Math.floor(Math.random() * mCount) - count);
+
+    return db.manga.findMany({
+        take: count,
+        skip: skip
+    })
+}
+
+function shuffleMangas(mangas: Manga[]): Manga[] {
+    // Deep copy the original array to avoid mutating the original array
+    const shuffledMangas = [...mangas];
+
+    // Fisher-Yates shuffle algorithm
+    for (let i = shuffledMangas.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [shuffledMangas[i], shuffledMangas[j]] = [shuffledMangas[j], shuffledMangas[i]];
+    }
+
+    return shuffledMangas;
+}
+
+
 
 export default RandomManga;
--- src/components/Manga/components/MangaCard.tsx
+++ src/components/Manga/components/MangaCard.tsx
@@ -2,6 +2,7 @@ import type { Manga } from "@prisma/client";
 import Image from "next/image";
 import { baseURL } from "@/utils/config";
 import MangaImage from "./MangaImage";
+import Link from "next/link";
 
 
 type MangaCardProps = {
@@ -12,16 +13,22 @@ const MangaCard: React.FC<MangaCardProps> = ({ manga }) => {
     return (
         <div className="shadow-md">
             <div className="grid grid-cols-[.5fr_1fr] gap-2 rounded-md bg-background/40">
-                <MangaImage
-                    priority
-                    sizes="(max-width: 640px) 21vw, 25vw"
-                    manga={manga}
-                />
+                <Link href={`/#`}>
+                    <MangaImage
+                        priority
+                        sizes="(max-width: 640px) 21vw, 25vw"
+                        manga={manga}
+                    />
+                </Link>
+
 
                 <div className="space-y-1.5 md:space-y-3 px-2 py-0.5 pb-1">
-                    <p className="text-xl md:text-2xl line-clamp-2 md:line-clamp-3 font-semibold">
-                        {manga.title}
-                    </p>
+                    <Link href={'/#'}>
+                        <p className="text-xl md:text-2xl line-clamp-2 md:line-clamp-3 font-semibold group-hover:text-2xl">
+                            {manga.title}
+                        </p>
+                    </Link>
+
                     <p className="line-clamp-3 max-sm:text-sm">{manga.description}</p>
                 </div>
             </div>
